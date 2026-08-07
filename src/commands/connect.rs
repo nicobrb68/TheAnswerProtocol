12b78f8cf1acdc03309a7cbfeb0998cececfd861
nbilyj <nbilyj@k2r3p9.42mulhouse.fr>
2026-08-07 12:41:06 +0200

    fix : adding user in room.players where room is world.spwan, on arrival

--- src/commands/connect.rs
+++ src/commands/connect.rs
@@ -8,9 +8,10 @@ pub async fn handle_connect(username: &str, world: &Arc<Mutex<World>>) -> String
     if w.has_player(&username) {
         return TapError::NameInUse.message();
     }
-    let user = Player::new(username.to_string(), w.spawn.clone());
+    let spawn = w.spawn.clone();
+    let user = Player::new(username.to_string(), spawn.clone());
     w.add_player(user);
-    w.get_mut_room("room.square").unwrap().players.push(username.to_string());
+    w.get_mut_room(&spawn).unwrap().players.push(username.to_string());
 
     "OK Connected\n".to_string()
 }
