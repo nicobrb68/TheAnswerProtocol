f39213561011a820424609d19a76ca6df32f2073
Nolhan-B <nolhanbil@gmail.com>
2026-08-09 16:38:40 +0200

    utilisation de notify room pour print les evt de mouvement

--- src/commands/movement.rs
+++ src/commands/movement.rs
@@ -3,6 +3,7 @@ use tokio::sync::{Mutex, MutexGuard};
 use crate::{World, Room, TapError};
 use std::collections::HashMap;
 use tokio::sync::mpsc::UnboundedSender;
+use crate::events::room::notify_room;
 
 pub async fn handle_move(username: &str, direction: &str, world: &Arc<Mutex<World>>, registry: &Arc<Mutex<HashMap<String, UnboundedSender<String>>>>) -> String {
     let mut w: MutexGuard<World> = world.lock().await;
@@ -17,20 +18,8 @@ pub async fn handle_move(username: &str, direction: &str, world: &Arc<Mutex<Worl
     w.get_mut_player(username).unwrap().current_room = new_room_id.clone();
     w.get_mut_room(&current_room).unwrap().players.retain(|p| p != username);
     w.get_mut_room(&new_room_id).unwrap().players.push(username.to_string());
-    let reg = registry.lock().await;
-    for player in &w.get_room(&current_room).unwrap().players {
-        if let Some(tx) = reg.get(player) {
-            if player != username {
-                let _ = tx.send(format!("EVT ROOM PRESENCE LEAVE {}\n", username));
-            }
-        }
-    }
-    for player in &w.get_room(&new_room_id).unwrap().players {
-        if let Some(tx) = reg.get(player) {
-            if player != username {
-                let _ = tx.send(format!("EVT ROOM PRESENCE ENTER {}\n", username));
-            }
-        }
-    }
+    drop(w);
+    notify_room(&current_room, &format!("EVT ROOM PRESENCE LEAVE {}\n", username), Some(username), &world, &registry).await;
+    notify_room(&new_room_id, &format!("EVT ROOM PRESENCE ENTER {}\n", username), Some(username), &world, &registry).await;
     format!("OK room={}\n", new_room_id)
 }
